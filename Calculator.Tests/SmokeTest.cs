8ef497c1da77b63bdcf87d80a8750cee5b83cde1
Sasha <sashapat1932@gmail.com>
2026-05-12 18:08:56 +0300

    first-try

--- Calculator.Tests/SmokeTest.cs
+++ Calculator.Tests/SmokeTest.cs
@@ -1,12 +1,21 @@
 ﻿using Xunit;
 
-namespace Calculator.Tests;
-
-public class SmokeTest
+namespace Calculator.Tests
 {
-    [Fact]
-    public void Application_Should_StartSuccessfully()
+    public class SmokeTests
     {
-        Assert.Equal(2, 2);
+        [Fact(DisplayName = "SmokeTest_VerifyOneEqualsOne")]
+        public void SmokeTest_VerifyOneEqualsOne()
+        {
+            Assert.Equal(1, 1);
+        }
+
+        [Fact(DisplayName = "SmokeTest_CanAddNumbers")]
+        public void SmokeTest_CanAddNumbers()
+        {
+            var calc = new CalculatorClass();
+            var result = calc.Add(1, 2);
+            Assert.Equal(3, result);
+        }
     }
 }
--- Calculator/obj/Debug/net10.0/Calculator.AssemblyInfo.cs
+++ Calculator/obj/Debug/net10.0/Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5de507030efbfe9c8ff33954bfd3b5cef41c0db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c741b0992c8de911fa5de0473dd8e9c966c012f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
